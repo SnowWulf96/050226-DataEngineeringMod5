b9263067cf61788be1d501b1e43b20d78e2451a9
kieran latham <kieran.latham@mab.org.uk>
2026-05-06 13:34:58 +0100

    calcs

--- Scientific_calculator.py
+++ Scientific_calculator.py
@@ -1,5 +1,12 @@
 from Calculator import Calculator
 
-MYCALCV2 = Calculator(5,20)
+#MYCALCV2 = Calculator(5,20)
 
-print(MYCALCV2.multiply())
+#print(MYCALCV2.multiply())
+
+class SciCalc(Calculator):
+    def get_exponent(self):
+        return self.a ** self.b
+    
+    mySciCalc = SciCalc(a=2, b=3)
+    print(mySciCalc.get_exponent())
